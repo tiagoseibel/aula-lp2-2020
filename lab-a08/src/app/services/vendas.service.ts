b634007bfa300db416c22d1863a5d9c87c97ea2c
Tiago Seibel <tiagoseibel@gmail.com>
2020-05-13 22:12:44 -0300

    angular model e service

---
+++ lab-a08/src/app/services/vendas.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+import axios from 'axios';
+import { Venda } from '../cadastros/vendas/venda-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VendasService {
+
+   url = "http://localhost:8080/vendas";
+
+   public findAll(): Promise<any> {
+      return axios.get(this.url);
+   }
+
+   public save(venda: Venda): Promise<any> {
+      if(venda.id > 0) {
+         return axios.put(this.url + "/" + venda.id, venda);
+      } else {
+         return axios.post(this.url, venda);
+      }      
+   }
+
+   public delete(venda_id: number): Promise<any> {
+      return axios.delete(this.url + "/" + venda_id);
+   }
+
+  constructor() { }
+}
